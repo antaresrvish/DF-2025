6d81c1c614b5d23343ad5031002050b71cf77b64
antaresrvish <yildirimyusuf758@gmail.com>
2025-03-22 13:43:12 +0300

    fix: bind Fastify server to all network interfaces

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -13,7 +13,7 @@ fastify.get('/', async (request, reply) => {
   return { message: 'CORS enabled!' };
 });
 
-fastify.listen({ port: 3000 }, (err, address) => {
+fastify.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
